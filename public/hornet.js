89683fbc5d623eaf15448d3b9b7d75828a14ef56
Robin Komiwes <odisseus@gmail.com>
2011-07-01 19:20:53 +0200

    update to socket.io 0.7.x, then bumped to hornet 0.2.0

--- public/hornet.js
+++ public/hornet.js
@@ -1,6 +1,5 @@
-var Hornet = function (url, port, channel, token){
-  this.url = url;
-  this.port = port;
+var Hornet = function (uri, channel, token){
+  this.uri = uri;
   this.token = token;
   this.channel = channel;
   this.handlers = [];
@@ -39,36 +38,35 @@ Hornet.prototype = {
   },
 
   connect : function () {
-    this.socket = new io.Socket( this.url, {port: this.port} );
+    this.socket = io.connect( this.uri );
     var socket = this.socket;
     var that = this;
 
-    socket.on('connect', function(){});
+    socket.on('connect', function(){
+      socket.on('message', function( rawMessage ) {
+        var message = JSON.parse( rawMessage );
 
+        if ( ! message.type )
+          return;
 
-    socket.on('message', function( rawMessage ) {
-      var message = JSON.parse( rawMessage );
+        var type = message.type;
+        var handlers = that.handlers;
 
-      if ( ! message.type )
-        return;
+        if ( handlers[type] == undefined )
+          return;
 
-      var type = message.type;
-      var handlers = that.handlers;
+        for ( i in handlers[type] ) {
+          var handler = handlers[type][i];
 
-      if ( handlers[type] == undefined )
-        return;
-
-      for ( i in handlers[type] ) {
-        var handler = handlers[type][i];
-
-        handler( message );
-      }
+          handler( message );
+        }
+      });
     });
+    
 
     socket.on('disconnect', function(){ 
     });
 
-    socket.connect();
     socket.send(JSON.stringify({token: this.token, channel: this.channel}));
   }
 }
