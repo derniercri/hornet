510c3a8bb05318238597a8a62dd97b3a67ddb3a3
Robin Komiwes <odisseus@gmail.com>
2011-11-20 16:08:05 +0100

    Large code refactor, lot more cleaner

--- public/hornet.js
+++ public/hornet.js
@@ -1,18 +1,22 @@
+var Hornet;
+
+(function() {
+
+var log = function( txt ) {
+  if (console && console.log )
+    console.log("Hornet: " + txt);
+}
+
 var delayedTimeout = function( callback, initialTime, maxTime ) {
   this.currentTime = initialTime;
   this.callback = callback;
   this.maxTime = 0 || maxTime;
 
   this.nextTick();
-}
+};
 
 delayedTimeout.prototype = {
-  stop : function () {
-    clearTimeout(this.timeout);
-  },
-
   nextTick : function() {
-
     if ( ! this.previousTime )
       this.previousTime = 0;
 
@@ -29,28 +33,34 @@ delayedTimeout.prototype = {
   },
 
   onTimeout : function( ctx ) {
-    var that = this;
+    var that = ctx ? ctx : this;
 
-    if ( ctx )
-      that = ctx;
-    
     that.nextTick();
-    
     that.callback(that);
+  },
 
-    
+  stop : function () {
+    clearTimeout(this.timeout);
   }
 }
 
-var Hornet = function (uri, channels, token){
+
+Hornet = function (uri, channels, token){
   if ( arguments.length == 1 && typeof arguments[0] == 'object' ) {
     var opts = arguments[0];
+
+    if ( ! opts['channels'] && ! opts['channel'] ) {
+      log("You must at least provide a channel (channel param)");
+      return;
+    }
 
     this.uri = opts['uri'];
     this.channels = opts['channels'] ? opts['channels'] : [ opts['channel'] ];
     this.token = opts['token'];
-  } else {
-    // DEPRECATED
+  }
+  else { // Deprecated
+    log("Using constructor with multiples params instead of json object is deprecated and will be removed in Hornet 0.4" );
+
     this.uri = arguments[0];
     this.channels = arguments[1];
     this.token = arguments[2];
@@ -60,23 +70,27 @@ var Hornet = function (uri, channels, token){
 }
 
 Hornet.prototype = {
+  /**
+   * The connection status, false/disconnected, true: connected
+   */
+  connected : false,
+
   addHandler : function(channel, type, callback) {
     var handlers = this.handlers;
 
-    if ( handlers[channel] == undefined )
+    if ( ! channel in handlers )
       handlers[channel] = {};
-    if ( handlers[channel][type] == undefined )
+
+    if ( ! type in handlers[channel] )
       handlers[channel][type] = [];
     
     handlers[channel][type].push( callback );
   },
-  //Connection status 
-  connected : false,
 
   removeHandler : function(channel, type, callback) {
     var handlers = this.handlers;
 
-    if ( handlers[channel] == undefined  && handlers[channel][type] )
+    if ( ( ! channel in handlers )  || ( ! type in handlers[channel] ) )
       return; // do nothing...
 
     for ( i in handlers[channel][type] ) {
@@ -84,7 +98,7 @@ Hornet.prototype = {
 
       if ( handler == callback ) {
         handlers[channel][type].splice(i, 1);
-        break;
+        return;
       }
     }
   },
@@ -94,7 +108,8 @@ Hornet.prototype = {
       for ( var i in channel ) {
         this.addHandler(channel[i], type, callback);
       }
-    } else {
+    } 
+    else {
       this.addHandler(channel, type, callback);
     }
   },
@@ -104,7 +119,8 @@ Hornet.prototype = {
       for ( var i in channel ) {
         this.removeHandler(channel, type, callback);
       }
-    } else {
+    } 
+    else {
       this.removeHandler(channel, type, callback);
     }
   },
@@ -114,26 +130,24 @@ Hornet.prototype = {
     var socket = this.socket;
     var that = this;
 
-
     socket.on('connect', function(){
-    
       that.connected = true;
-      
       socket.send(JSON.stringify({token: that.token, channels: that.channels}));
-
     });
     
     socket.on('message', function( rawMessage ) {
       var message = JSON.parse( rawMessage );
 
-      if ( ! message.type && ! message.channel )
+      if ( ! message.type && ! message.channel ) {
+        log("Wrong message format. Received: " + JSON.stringify(message) );
         return;
+      }
 
       var type = message.type;
       var channel = message.channel;
       var handlers = that.handlers;
 
-      if ( handlers[channel] == undefined || handlers[channel][type] == undefined )
+      if ( ! channel in handlers || ! type in handlers[channel] )
         return;
 
       for ( i in handlers[channel][type] ) {
@@ -147,22 +161,24 @@ Hornet.prototype = {
       var handlers = that.handlers;
       that.connected = false;
       
-      if ( handlers['hornet']['disconnect'] != undefined )
-      {
+      if ( "disconnect" in handlers['hornet'] != undefined )  {
         for ( i in handlers['hornet']['disconnect'] ) {
           var handler = handlers['hornet']['disconnect'][i];
           handler();
         }
       }
       
-      var reconnect = new delayedTimeout( function(self){
+      var reconnect = new delayedTimeout( function( self ) {
         if( ! that.connected )
           that.socket.socket.connect();
         else
           self.stop();
-      }, 5000, 30000);
+      }, 1000, 30000); // will try to reconnect after 1sec and will up to 30sec
     });
-
-    
   }
-}
+};
+
+  
+});
+
+var $h = Hornet;
