e7222205175e22c34608c20c30ac91f41d17da99
Maxime Brazeilles <maxime.brazeilles@gmail.com>
2018-09-07 08:26:40 +0200

    Due to some legacy... let's keep the code intact just prettify it

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
 /*!
  * Hornet
  * Copyright(c) 2011 Nectify <dev@nectify.com>
  * Apache 2.0 Licensed
  */
 
-module.exports = require('./lib/server');
+module.exports = require("./lib/server");
--- test/static_routes.test.js
+++ test/static_routes.test.js
@@ -4,116 +4,112 @@
  * Apache 2.0 Licensed
  */
 
- var consts = require('../lib/consts')
-  , http = require('http')
-  , c = require('./common')
-  , should = require('should');
+var consts = require("../lib/consts"),
+  http = require("http"),
+  c = require("./common"),
+  should = require("should");
 
-
-describe('Hornet static routes', function() {
-  describe('GET /', function() {
-    it('should return hornet welcome message', function( done ) {
-
-      c.hornetReadyUp( function( hornet ) { 
+describe("Hornet static routes", function() {
+  describe("GET /", function() {
+    it("should return hornet welcome message", function(done) {
+      c.hornetReadyUp(function(hornet) {
         var options = {
-          host: 'localhost',
-          port: hornet.settings['port'],
-          path: '/',
-          method: 'GET'
+          host: "localhost",
+          port: hornet.settings["port"],
+          path: "/",
+          method: "GET"
         };
 
         var body = "";
 
-        var req = http.request(options, function(res) { 
-          res.setEncoding('utf8');
+        var req = http.request(options, function(res) {
+          res.setEncoding("utf8");
 
-          res.on('data', function (chunk) {
+          res.on("data", function(chunk) {
             body = body + chunk;
           });
 
-          res.on('end', function() {
+          res.on("end", function() {
             hornet.close();
-             
+
             res.should.have.status(200);
-            body.should.include('Hornet v');
-            
+            body.should.include("Hornet v");
+
             done();
           });
-        });  
-        
-        req.end();    
+        });
+
+        req.end();
       });
     });
   });
 
-  describe('GET root', function() {
-    it('should return hornet welcome message', function( done ) {
-
-      c.hornetReadyUp( function( hornet ) { 
+  describe("GET root", function() {
+    it("should return hornet welcome message", function(done) {
+      c.hornetReadyUp(function(hornet) {
         var options = {
-          host: 'localhost',
-          port: hornet.settings['port'],
-          path: '',
-          method: 'GET'
+          host: "localhost",
+          port: hornet.settings["port"],
+          path: "",
+          method: "GET"
         };
 
         var body = "";
 
-        var req = http.request(options, function(res) { 
-          res.setEncoding('utf8');
+        var req = http.request(options, function(res) {
+          res.setEncoding("utf8");
 
-          res.on('data', function (chunk) {
+          res.on("data", function(chunk) {
             body = body + chunk;
           });
 
-          res.on('end', function() {
+          res.on("end", function() {
             hornet.close();
 
             res.should.have.status(200);
-            body.should.include('Hornet v');            
+            body.should.include("Hornet v");
 
             done();
           });
-        });  
-        
-        req.end();    
-      })
+        });
+
+        req.end();
+      });
     });
   });
 
-  describe('GET hornet js file', function() {
-    it('should return hornet js file', function( done ) {
-      
-      c.hornetReadyUp( function( hornet ) { 
+  describe("GET hornet js file", function() {
+    it("should return hornet js file", function(done) {
+      c.hornetReadyUp(function(hornet) {
         var options = {
-          host: 'localhost',
-          port: hornet.settings['port'],
-          path: '/hornet/hornet.js',
-          method: 'GET'
+          host: "localhost",
+          port: hornet.settings["port"],
+          path: "/hornet/hornet.js",
+          method: "GET"
         };
 
         var body = "";
-        var req = http.request(options, function(res) { 
-          res.setEncoding('utf8');
+        var req = http.request(options, function(res) {
+          res.setEncoding("utf8");
 
-          res.on('data', function (chunk) {
+          res.on("data", function(chunk) {
             body = body + chunk;
           });
 
-          res.on('end', function() {
+          res.on("end", function() {
             hornet.close();
 
             res.should.have.status(200);
 
-            body.should.include('var Hornet');
-            body.should.include('Socket.IO.min.js');
-            
+            body.should.include("var Hornet");
+            body.should.include("Socket.IO.min.js");
+
             done();
           });
-        });  
-        
-        req.end();    
-      })
+        });
+
+        req.end();
+      });
     });
   });
 });
